6ced8dc61555051f89b7c7580d59c88475603679
zhaocharlie66 <zhaocharlie66@gmail.com>
2026-03-11 23:05:07 +0800

    Create user_agents.js

---
+++ user_agents.js
@@ -0,0 +1,9 @@
+module.exports = [
+
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_6) AppleWebKit/537.36 Chrome/119.0 Safari/537.36",
+"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/118.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/121.0 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 12_7) AppleWebKit/537.36 Chrome/117.0 Safari/537.36"
+
+]
